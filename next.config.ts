c5f299d8f1eff89ebc390ecaf8935eaa6d3eccc7
adielBm <adiel.bm5@gmail.com>
2026-04-21 14:33:18 +0300

    path

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/songbook',
+  assetPrefix: '/songbook',
   trailingSlash: true,
   images: {
     unoptimized: true,
